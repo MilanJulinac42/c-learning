cfc510b0395b06c2248cfd45f04b6f61383363ba
milan julinac <milanjulinac996@gmail.com>
2024-01-27 09:37:56 +0100

    first project

--- file.c
+++ file.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World");
-    printf("Hello World");
-    printf("Hello World");
-    printf("Hello World");
+
+    char name[] = "Petar";
+    int age = 20;
+
+    printf("Hello %s\n", name);
+    printf("Hello %i\n", age);
+    age = 21;
+    printf("Hello %i\n", age);
+    printf("Hello World\n");
+
     return 0;
 }
