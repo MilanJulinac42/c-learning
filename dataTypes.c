835875202eeecf596d0a4b6db5605af9a70742eb
milan julinac <milanjulinac996@gmail.com>
2024-01-27 09:49:54 +0100

    printf

---
+++ dataTypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+    int age = 40;
+    double gpa = 3.7;
+    char grade = 'A';
+    char phrase[] = "Milan Julinac";
+
+
+    return 0;
+}
---
+++ printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+
+    int favNum = 90;
+    printf("%s %d %f %d","number" , 500, 46.2131, favNum);
+
+    return 0;
+
+}
