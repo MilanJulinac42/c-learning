b26af7c78606b463cf26a77883aeb998de81758c
milan julinac <milanjulinac996@gmail.com>
2024-01-27 09:57:53 +0100

    numbers

---
+++ numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+    int num = 6;
+    printf("%f", pow(2, 3));
+    printf("%f", sqrt(36));
+    printf("%f", ceil(36.356));
+    printf("%f", floor(36.656));
+
+    return 0;
+}
